679a6eba5928685c8086159ad29b2106e528bc62
Alena Radomskaia <radomskaia.a.i@gmail.com>
2024-07-27 03:10:44 +0700

    feat: add check()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+
+    let previousStr;
+    do {
+        previousStr = str;
+        bracketsConfig.forEach((bracket) => {
+            str = str.replace(bracket.join(''), '');
+        })
+    } while (previousStr !== str)
+
+    return str.length === 0;
 }
